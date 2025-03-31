ff5a8fea59584ed9a347dbf6fc7cf0447e540d08
AllFox <tigraneganan1@gmail.com>
2025-03-31 19:52:28 +0300

    add SurveyDTO

---
+++ SurveyProject.Core/SurveyDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyProject.Core
+{
+    internal class SurveyDTO
+    {
+    }
+}
