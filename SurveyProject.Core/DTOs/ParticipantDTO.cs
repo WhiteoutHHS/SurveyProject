532ec81526e5ab79c22887e7898c8b90a20c23b4
AllFox <tigraneganan1@gmail.com>
2025-04-08 20:03:27 +0300

    edit DTOs

--- SurveyProject.Core/DTOs/ParticipantDTO.cs
+++ SurveyProject.Core/DTOs/ParticipantDTO.cs
@@ -8,14 +8,10 @@ namespace SurveyProject.Core.DTOs
 {
     public class ParticipantDTO
     {
-<<<<<<< HEAD
         public UserDTO Creator { get; set; }
         public RoomDTO Room { get; set; }
         public int ParticipantType { get; set; }
-=======
-        public RoomDTO Room {  get; set; }
         public UserDTO User { get; set; }
         public int ParticipantId { get; set; }
->>>>>>> 6d4466f (edit DTOs)
     }
 }
